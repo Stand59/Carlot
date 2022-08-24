bfbcd00d8f30d36ead2db242d207da9c1ca1f190
NathanStanding <standingtall1000@gmail.com>
2022-08-24 14:02:52 -0600

    changes of alignment

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,8 +1,17 @@
 import App from "../App";
+import { Navbar, Nav, NavItem, NavLink, NavbarBrand } from 'reactstrap';
 
 const SideNav = () => {
     return(
-        <div>sideNav</div>
+        <div className='sidenav'>
+            <a href="#">About</a>
+            <br></br>
+            <a href="#">Services</a>
+            <br></br>
+            <a href="#">Clients</a>
+            <br></br>
+            <a href="#">Contact</a>
+        </div>
     );
 };
 
