7fcba6f121e156368f42daaec53f0b6d94cc26d4
NathanStanding <standingtall1000@gmail.com>
2022-09-01 00:04:44 -0600

    added new car listings

--- src/components/CarInfo.js
+++ src/components/CarInfo.js
@@ -1,7 +1,21 @@
 import Card from "./Card";
-import corvette from "../assets/corvette.jpg"
-import mustang from "../assets/download.jpg"
-import vw from "../assets/download-1.jpg"
+import corvette from "../assets/corvette.jpg";
+import mustang from "../assets/download.jpg";
+import vw from "../assets/download-1.jpg";
+import minivan from "../assets/minivan.jpg";
+import Jetta from "../assets/jetta.jpg";
+import armor from "../assets/armor.jpg";
+import bavan from "../assets/bavan.jpg";
+import deathstar from "../assets/deathstar.jpg";
+import flintstone from "../assets/flintstone.jpg";
+import helicopter from "../assets/helicopter.jpg";
+import herse from "../assets/herse.jpg";
+import limo from "../assets/limo.jpg";
+import lowrider from "../assets/lowrider.jpg";
+import magic from "../assets/magic.jpg";
+import slugbug from "../assets/slugbug.jpg";
+
+
 import '../App.css';
 import { Container, Row, Col } from "reactstrap";
 
@@ -26,75 +40,75 @@ const cars = [
     },
     {
         id:3,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Minivan",
+        cost: 13000,
+        image: minivan
     },
     {
         id:4,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Jetta TDI",
+        cost: 8000,
+        image: Jetta
     },
     {
         id:5,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Slugbug",
+        cost: 14932,
+        image: slugbug
     },
     {
         id:6,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Limosine",
+        cost: 50000,
+        image: limo
     },
     {
         id:7,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Heli-car",
+        cost: 400000000,
+        image: helicopter
     },
     {
         id:8,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "B.A. Van",
+        cost: 789888,
+        image: bavan
     },
     {
         id:9,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "armored car",
+        cost: 3000000,
+        image: armor
     },
     {
         id:10,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Magic School Bus",
+        cost: 4100000,
+        image: magic
     },
     {
         id:11,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Flintstone",
+        cost: -10,
+        image: flintstone
     },
     {
         id:12,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Herse",
+        cost: 20444,
+        image: herse
     },
     {
         id:13,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Death Star",
+        cost: '999999999',
+        image: deathstar
     },
     {
         id:14,
-        name: "VW",
-        cost: 4000000,
-        image: vw
+        name: "Low Rider",
+        cost: 8050,
+        image: lowrider
     },
 ];
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({car}) => {
     const { image, name, cost} = car;
     return (
         <div className="card" onClick= 'clickMe()'>
-                <img src={image} alt={name}></img>
+                <img height='150px' src={image} alt={name}></img>
             <div className="container">
                 <h4><b>{name}</b></h4>
                 <p>Price: ${cost}</p>
